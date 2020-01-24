104cf876de65e869bf3e239e4c6b59c922c7e4ad
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    extra: Use Pixel charger animation
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Change-Id: I89ac5aa6c4dec0ded4524e94dc07fd268690b5f4
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- config.mk
+++ config.mk
@@ -4,6 +4,10 @@
 # Build system
 BUILD_BROKEN_DUP_RULES := true
 
+# Charger
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/charger,$(TARGET_COPY_OUT_PRODUCT)/etc/res)
+
 # START PREBUILT APK #
 
 # CameraGO
