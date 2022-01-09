4403b82e7f3df8fe98c8e362edffc4293b35d4e9
officialputuid <officialputuid@hack.id>
2022-01-09 15:52:40 +0800

    extra: prebuilt: apk: Update XRecorder
    * versionCode: 220102 && versionName: 2.2.0.1
    
    Signed-off-by: officialputuid <officialputuid@hack.id>
    Change-Id: Ia90ce78b6e232bb097a579d200d621d1a945a247

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -69,7 +69,7 @@ LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
 # XRecorder
-# versionCode: 212101 && versionName: 2.1.2.1
+# versionCode: 220102 && versionName: 2.2.0.1
 include $(CLEAR_VARS)
 LOCAL_MODULE := XRecorder
 LOCAL_SRC_FILES := apk/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
