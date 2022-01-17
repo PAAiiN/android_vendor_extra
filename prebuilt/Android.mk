9b1c36f787792ef5472e117fa85ff808ff0e27b8
officialputuid <officialputuid@hack.id>
2022-01-17 21:38:21 +0800

    extra: prebuilt: apk: Update GrapheneOS Camera
    * versionCode: 9 && versionName: 9
    
    Signed-off-by: officialputuid <officialputuid@hack.id>
    Change-Id: I58c7432ead85c835fb03487b0495fe5eb4a2547b

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -30,7 +30,7 @@ LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Snap Camera2
 include $(BUILD_PREBUILT)
 
 # GrapheneOSCamera
-# versionCode: 8 && versionName: 8
+# versionCode: 9 && versionName: 9
 include $(CLEAR_VARS)
 LOCAL_MODULE := GrapheneOSCamera
 LOCAL_SRC_FILES := apk/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
